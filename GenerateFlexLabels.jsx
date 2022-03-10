947b1323ada4eff29d22e4c53065a1dccbbffa0e
ajautz@gmail.com <ajautz@gmail.com>
2022-03-09 21:20:37 -0500

    script is hotttt. Making it 5 digits for serial. Falls off the artboard but not a big deal.

--- GenerateFlexLabels.jsx
+++ GenerateFlexLabels.jsx
@@ -1,69 +1,83 @@
 ﻿// create new document/file
-if(!app.homeScreenVisible) {
-    app.activeDocument.close(SaveOptions.DONOTSAVECHANGES);
-    }
+if (!app.homeScreenVisible) {
+  app.activeDocument.close(SaveOptions.DONOTSAVECHANGES);
+}
 
-    // main code
-    var docPreset = new DocumentPreset; 
-    docPreset.width = 600; 
-    docPreset.height = 500; 
-    docPreset.units = RulerUnits.Millimeters; 
-    docPreset.artboardLayout = DocumentArtboardLayout.GridByCol; 
-    docPreset.numArtboards = 1 
-    var myDocument = app.documents.addDocument(DocumentColorSpace.CMYK, docPreset);
+// main code
+var docPreset = new DocumentPreset();
+docPreset.width = 600;
+docPreset.height = 500;
+docPreset.units = RulerUnits.Millimeters;
+docPreset.artboardLayout = DocumentArtboardLayout.GridByCol;
+docPreset.numArtboards = 1;
+var myDocument = app.documents.addDocument(DocumentColorSpace.CMYK, docPreset);
+myDocument.layers[0].name = "Background Layer";
+var layerTwo = myDocument.layers.add();
+layerTwo.name = "Label Layer";
+var layerThree = myDocument.layers.add();
+layerThree.name = "Text Layer";
 
+// parameters
+var qty = 100;
 
-    // var myDocument = app.documents.add();
-    myDocument.layers[0].name = "Background Layer";
+// loop to create labels
+for (var i = 0; i < qty; i++){
+  // create rectangle
+  generateLabel(i);
 
-    // create solid coloured bg
-    // generateBackground();
+  // create text
+  generateText(i);
 
-    // create a layer for a triangle with just stroke
-    var layerTwo = myDocument.layers.add();
-    layerTwo.name = "Label Layer";
-    generateLabel();
+  // import QR from file
+  importQR(i);
+}
 
-    // create a new layer for the text
-    var layerThree = myDocument.layers.add();
-    layerThree.name = "Text Layer";
-    generateText("SATFF020001");
+function generateText(i) {
+  var doc = app.activeDocument;
+  var textFrame = doc.textFrames.add();
+  textFrame.name = "Test Text Frame";
+  // .contents to change text
+  // first build the serial number, need to have right number of digits
+  var realSerial = i + 1;
+  var serialVal = ("00000" + realSerial).slice(-5); 
+  textFrame.contents = "SATFF02"+serialVal;
+  var textRange = textFrame.textRange;
+  textRange.size = 5.8;
+  textRange.justification = Justification.CENTER;
+  textFrame.position = [103, 92-i*30];
+}
 
-    // save out png file of automated creation
-    // myDocument.exportFile(File("~/Documents/test.png"), ExportType.PNG24);
-    
+function generateLabel(i) {
+  var doc = app.activeDocument;
+  var rect = doc.pathItems.roundedRectangle(
+    100-(i*30),
+    100,
+    20.8 / 0.3527,
+    7.8 / 0.3527,
+    1,
+    1
+  );
+  rect.strokeColor = makeColor(75, 0, 100, 0);
+  rect.strokeWidth = 0.5;
+}
 
-function generateBackground() {
-        var doc = app.activeDocument;
-        var background = doc.pathItems.add();
-        background.filled = true;
-        var bgFillColour = new CMYKColor();
-        bgFillColour.cyan = 4;
-        bgFillColour.magenta = 2;
-        bgFillColour.yellow = 97;
-        bgFillColour.black = 1;
-        background.fillColor = bgFillColour;
-        background.setEntirePath([[0, 0], [doc.width, 0], [doc.width, doc.height], [0, doc.height]]);
-    }
+function importQR(i) {
+  var doc = app.activeDocument;
+//   var newLayer = doc.layers.add();
+//   newLayer.name = "QR Layer";
+  var imagePlacedItem = doc.placedItems.add();
+  imagePlacedItem.file = File(
+    "~/Documents/Programming/Illustrator/ai-basic-script/QR/("+(i+1)+").eps"
+  );
+  imagePlacedItem.resize(22.5, 22.5);
+  imagePlacedItem.position = [137, 99.5-i*30];
+}
 
-function generateText(text) {
-        var doc = app.activeDocument;
-        var textFrame = doc.textFrames.add();
-        textFrame.name = "Test Text Frame";
-        // .contents to change text
-        textFrame.contents = text;
-        var textRange = textFrame.textRange;
-        textRange.size = 18;
-        textRange.justification = Justification.CENTER;
-        textFrame.position = [110, 80];
-    }
-
-function generateLabel() {
-        var doc = app.activeDocument;
-        // var rectangle = doc.pathItems.add();
-        // rectangle.stroked = true;
-        // rectangle.setEntirePath([[10, 10], [10, 100], [250, 100], [250, 10], [10, 10]]);
-
-        // var rect = doc.pathItems.rectangle( 100, 100, 200, 50 );
-        var rect = doc.pathItems.roundedRectangle(100, 100, 200, 50, 3 ,3);
-    }
+function makeColor(c, m, y, k) {
+  var color = new CMYKColor();
+  color.cyan = c;
+  color.magenta = m;
+  color.yellow = y;
+  color.black = k;
+  return color;
+}
